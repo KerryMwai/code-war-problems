6703b3b899ddc4eb4cda49f221ec07e93903e1bf
KerryMwai <kerrywangamati@gmail.com>
2023-06-21 10:55:10 +0300

    Calculation factorila and throwing a range error if the integer specified is out of range required

--- factorial.dart
+++ factorial.dart
@@ -1,8 +1,10 @@
 int findFactorial(int n){
+  if(n<0) throw RangeError("The value is less than 0");
+  if(n>12) throw RangeError("The value is greater than 12");
   if(n==0 || n==1) return 1;
   return n*findFactorial(n-1);
 }
 
 void main(){
-    print(findFactorial(2));
+    print(findFactorial(13));
 }
