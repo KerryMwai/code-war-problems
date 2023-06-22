a3386d79ff6dd60d8001ce86fe355fe34182e581
KerryMwai <kerrywangamati@gmail.com>
2023-06-22 14:40:44 +0300

    Palindrome

--- palindrome.dart
+++ palindrome.dart
@@ -1,14 +1,18 @@
-void main(){
-  String w="12/21/33 12:21";
-  List<String> w2=w.split('');
-  List<String> w3=[];
+bool isPalindrome(String x) {
+    List<String> w2=x.split('');
+    List<String> w3=[];
   for(var word in w2.reversed){
     w3.add(word);
   }
-
-  if(w3.join('')==w){
-      print("They are equal");
+   if(w3.join('').toLowerCase()==x.toLowerCase()){
+      return true;
   }else{
-    print("They are not equal");
+    return false;
   }
+}
+void main(){
+  String w="12/21/33 12:21";
+  isPalindrome(w)?print("The word is equal to its inverse"):print("The word is not equal to its inverse");
+    
+  
 }
